da819c7e0abb4719fba54dd5fb31f0a73ed9d59c
xphoenixua <uzya03@gmail.com>
2025-04-16 20:29:28 +0300

    Code clarity changes

--- src/tracking/point_of_gaze.py
+++ src/tracking/point_of_gaze.py
@@ -37,9 +37,10 @@ class PointOfGaze:
         self.nb_interv = 3  # allowed intervening inconsistencies
         self.cluster_min_size = 2  # minimum cluster size to be considered stable
         self.cluster_max_size = 20  # maximum cluster storage size
+        self.norm_threshold = 0.7 # normalized distance threshold for cluster membership
         # maximum allowed variation within a cluster (20% of screen width)
         self.max_intra_cluster_dist_x = 0.2 * self.monitor['width']
-        self.max_intra_cluster_dist_y = 0.2 * self.monitor['height']
+        self.max_intra_cluster_dist_y = 0.1 * self.monitor['height']
 
     def _init_cluster_storage(self):
         """
@@ -51,6 +52,7 @@ class PointOfGaze:
         self.candidate_cluster_y = deque(maxlen=self.cluster_max_size)
         self.move_cluster_x = deque(maxlen=self.cluster_max_size)
         self.move_cluster_y = deque(maxlen=self.cluster_max_size)
+        self.num_clusters = 0 # FOR DEBUGGING
 
     def _init_movement_flags(self):
         """
@@ -83,7 +85,7 @@ class PointOfGaze:
         when the user is looking straight ahead.
         """
         if self.looking_straight_ahead(raw_x, raw_y, self.gaze_calibration):
-            self.current_iris_size = self.gaze_calibration.measure_iris_diameter()
+            self.current_iris_size = self.gaze_calibration._measure_iris_diameter()
             self.logger.debug(f'iris updated to {self.current_iris_size}')
 
     def _compute_raw_gaze(self):
@@ -92,7 +94,6 @@ class PointOfGaze:
             return None
 
         if getattr(self, 'current_iris_size', None) in [None, 0]:
-            print('current iris size not valid, setting to baseline value')
             self.current_iris_size = self.gaze_calibration.base_iris_size
             self.logger.debug(f'iris set to baseline {self.current_iris_size}')
 
@@ -208,6 +209,9 @@ class PointOfGaze:
             self.current_cluster_x.append(x)
             self.current_cluster_y.append(y)
         else:
+            # Starting a new candidate cluster if none exists yet.
+            if not self.candidate_cluster_x:
+                self.num_clusters += 1
             self.candidate_eye_move = True
             self.move_cluster_x.append(x)
             self.move_cluster_y.append(y)
@@ -241,6 +245,7 @@ class PointOfGaze:
         self.candidate_cluster_y.clear()
         self.move_cluster_x.clear()
         self.move_cluster_y.clear()
+        self.num_clusters += 1
 
     def _point_within_cluster(self, cluster_x, cluster_y, x, y):
         """
@@ -269,15 +274,13 @@ class PointOfGaze:
         :param y: new y coordinate.
         :return: True if the normalized Euclidean distance for each point is within threshold.
         """
-        norm_threshold = 0.75
-
         cluster_x, cluster_y = [p[0] for p in cluster_points], [p[1] for p in cluster_points]
         mean_x = sum(cluster_x) / len(cluster_x)
         mean_y = sum(cluster_y) / len(cluster_y)
         dx = abs(mean_x - x) / self.monitor['width']
         dy = abs(mean_y - y) / self.monitor['height']
         norm_diff = np.sqrt(dx**2 + dy**2)
-        return norm_diff <= norm_threshold  # use your chosen threshold
+        return norm_diff <= self.norm_threshold  # use your chosen threshold
 
         # alpha, beta = 1.0, 1.0
         # for cx, cy in cluster_points:
@@ -312,19 +315,19 @@ class PointOfGaze:
         )
         return inconsistencies <= self.nb_interv
 
-    @staticmethod
-    def looking_straight_ahead(est_x, est_y, gaze_calib):
+    def looking_straight_ahead(self, est_x, est_y):
         """
         check if the estimated gaze is in the center region of the screen.
         
         :param est_x: estimated x coordinate.
         :param est_y: estimated y coordinate.
-        :param gaze_calib: calibration object containing screen dimensions.
         :return: true if gaze is in the center region, false otherwise.
         """
-        wmargin = gaze_calib.frame_width * 0.3
-        hmargin = gaze_calib.frame_height * 0.5
-        wmiddle = gaze_calib.frame_width / 2
-        hmiddle = gaze_calib.frame_height / 2
+        wmargin = self.frame_width * 0.3
+        hmargin = self.frame_height * 0.5
+        wmiddle = self.frame_width / 2
+        hmiddle = self.frame_height / 2
         return (wmiddle - wmargin < est_x < wmiddle + wmargin) and (hmiddle - hmargin < est_y < hmiddle + hmargin)
     
+    def get_num_clusters(self):
+        return self.num_clusters
